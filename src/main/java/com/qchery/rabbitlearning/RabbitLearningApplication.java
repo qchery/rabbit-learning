fa6e490a013ea9ab36a04ae507165436d1959f1e
Chery <chinrui1016@163.com>
2018-08-13 22:51:39 +0800

    initial project, integrated with web、amqp starters

---
+++ src/main/java/com/qchery/rabbitlearning/RabbitLearningApplication.java
@@ -0,0 +1,12 @@
+package com.qchery.rabbitlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitLearningApplication.class, args);
+	}
+}
